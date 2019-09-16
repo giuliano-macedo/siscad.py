5d4ccf9651ff67d67a5b61d91e948f29076b48b8
Giuliano Oliveira <giuliano.llpinokio@gmail.com>
2019-09-16 08:49:29 -0400

    download_url

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setuptools.setup(
 	 author_email="giuliano.llpinokio@gmail.com",
 	 description="Module for siscad.ufms.br",
 	 long_description=open("README.md").read(),
+	 download_url="https://github.com/llpinokio/siscad.py/archive/v0.0.1.tar.gz",
 	 long_description_content_type="text/markdown",
 	 keywords=["web-scraping"],
 	 url="https://github.com/llpinokio/siscad.py",
