fad9ab0534fbab2cd6f4133f4d57dc2145f26408
Giuliano Oliveira <giuliano.llpinokio@gmail.com>
2019-09-17 20:10:16 -0400

    mt update for Semestre

--- siscad/model/Semestre.py
+++ siscad/model/Semestre.py
@@ -1,4 +1,10 @@
+from multiprocessing.dummy import Pool
 class Semestre:
 	def __init__(self,date,discs):
 		self.date=date
 		self.discs=discs
+	def update(self,no_jobs=None):
+		pool=Pool(len(self.discs)) if no_jobs==None else Pool(no_jobs)
+		for _ in pool.imap(lambda disc:disc.update(),self.discs):
+			pass
+
